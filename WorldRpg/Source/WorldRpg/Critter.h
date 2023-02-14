a0d093edeced0082224b66cf92cbef2745dfb0ef
JinWoo_ <829kong@naver.com>
2023-02-14 10:07:23 +0900

    2023-02-14 초기 업데이트

---
+++ WorldRpg/Source/WorldRpg/Critter.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Critter.generated.h"
+
+UCLASS()
+class WORLDRPG_API ACritter : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ACritter();
+
+	UPROPERTY()
+	class UStaticMeshComponent* m_MeshComp;
+
+	UPROPERTY()
+	class UCameraComponent* m_Camera;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+
+};
---
+++ WorldRpg/Source/WorldRpg/WorldRpgGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WorldRpgGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WORLDRPG_API AWorldRpgGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
